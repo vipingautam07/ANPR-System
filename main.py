21a975dfae3af94c435a021c1f016034cbc0e1e4
Vipin Gautam <gautamvipin2002@gmail.com>
2024-04-06 23:57:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from cv2 import VideoCapture
+import cv2
+platecascade = cv2.CascadeClassifier("D:\haarcascade_russian_plate_number.xml")
+minArea = 500
+cap = cv2.VideoCapture(0)
+count = 0
+
+while True:
+    success,img=cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA) 
+
+    numberPlates = platecascade .detectMultiScale(imgGray, 1.1, 4)
+
+    for (x, y, w, h) in numberPlates:
+        area = w*h
+        if area > minArea:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (1080, 1920, 0), 2)
+            cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+            imgRoi = img[y:y+h,x:x+w]
+            cv2.imshow("ROI",imgRoi)
+    cv2.imshow("RESULT", img)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("D:\Z Number Plate\IMAGES"+str(count)+".jpg",imgRoi)
+        cv2.rectangle(img,(0,200),(640,300),(1080, 1920, 0),cv2.FILLED)
+        cv2.putText(img,"SCAN SAVED",(15,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,255,255),2)
+        cv2.imshow("FINAL OUTPUT",img)
+        cv2.waitKey(500)
+        count = count + 1
